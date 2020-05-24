d8718c2c1abf1383689dec8633b3f72bf05712b4
aniket.shashikant <aniket.shashikant@hashedin.com>
2020-05-25 01:02:16 +0530

    feat: Added individual deck card component for the AllDeckViews component

---
+++ components/Deck.js
@@ -0,0 +1,44 @@
+import React, { Component } from "react"
+import { StyleSheet, Text, TouchableOpacity, View, Platform } from "react-native"
+import { white, blue, gray, black } from '../utils/colors'
+
+export default class Deck extends Component {
+
+    render() {
+        const { title, numberOfCards } = this.props
+        return (
+            <View style={styles.container}>
+                <Text style={styles.title}>{title}</Text>
+                <Text >{numberOfCards} {numberOfCards < 2 ? "card" : "cards"}</Text>
+            </View>
+        )
+
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: gray,
+        borderRadius: Platform.OS === 'ios' ? 16 : 5,
+        padding: 20,
+        marginLeft: 40,
+        marginRight: 40,
+        marginTop: 12,
+        marginBottom: 5,
+        justifyContent: 'center',
+        alignItems: 'center',
+        shadowRadius: 3,
+        shadowOpacity: 0.8,
+        shadowColor: 'rgba(0, 0, 0, 0.24)',
+        shadowOffset: {
+            width: 0,
+            height: 3
+        },
+    },
+    title: {
+        fontSize: 30,
+        fontWeight: "bold"
+    }
+})
+
+
