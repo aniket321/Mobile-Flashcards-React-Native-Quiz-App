80cec3007e3c6eb1caa763c7a904d79c53e11fad
aniket.shashikant <aniket.shashikant@hashedin.com>
2020-05-25 10:53:50 +0530

    feat: Added screen to add a new deck

---
+++ components/AddDeck.js
@@ -0,0 +1,80 @@
+import * as React from 'react';
+import {
+    Text,
+    View,
+    StyleSheet,
+    TextInput,
+} from "react-native"
+import { addNewDeck } from "../utils/api"
+import { connect } from "react-redux"
+import { addDeck } from "../actions"
+import { white, black } from "../utils/colors"
+import Button from './Button'
+
+class AddDeck extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = { title: "" }
+    }
+
+    submit = () => {
+        const deck = this.state
+        this.setState(() => ({
+            title: "",
+        }))
+
+        addNewDeck(deck)
+        this.props.dispatch(addDeck(deck))
+
+    }
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text style={styles.title}>
+                    Enter Title of the Deck
+                </Text>
+                <TextInput
+                    style={styles.input}
+                    onChangeText={title => this.setState({ title })}
+                    value={this.state.title}
+                />
+                <Button
+                    onPress={this.submit}
+                    disabled={this.state.title === ""}
+                    btnText="Submit"
+                />
+            </View>
+        )
+    }
+}
+
+
+const styles = StyleSheet.create({
+    container: {
+        padding: 5,
+        height: "100%",
+        backgroundColor: white,
+        justifyContent: "center"
+    },
+    title: {
+        fontSize: 35,
+        margin: 10,
+        marginBottom: 35,
+        alignSelf: "center",
+        fontWeight: "600"
+    },
+    input: {
+        height: 60,
+        marginBottom: 35,
+        marginLeft: 20,
+        marginRight: 20,
+        fontSize: 22,
+        borderRadius: 4,
+        borderStyle: "solid",
+        borderWidth: 2,
+        borderColor: black,
+    }
+})
+
+export default connect()(AddDeck)
